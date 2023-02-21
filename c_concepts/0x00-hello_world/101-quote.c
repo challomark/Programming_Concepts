869bff0fb4e0c60d4db3d47bee325d33d16c466f
challomark <challomark@gmail.com>
2023-02-21 08:25:01 +0300

    Finished the assignment

---
+++ c_concepts/0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: to write a quote
+ *
+ * Return: 1
+ */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
+}
