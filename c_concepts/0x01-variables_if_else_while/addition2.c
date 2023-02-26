bdfb5c9d805a51afbf324ded02ed1b6ba2d351e5
challomark <challomark@gmail.com>
2023-02-26 08:33:28 +0300

    Using arithmetic and short hand operators

---
+++ c_concepts/0x01-variables_if_else_while/addition2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using arithmetic and short hand operators
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a = 5;
+
+	/* a = a + 3; */
+	a += 3;
+	/* a = a - 2; */
+	/* a -= 2; */
+
+	printf("%d\n", a);
+	return (0);
+}
