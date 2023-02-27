b46acc6ad6503dedcfc7ed4eeeee5e537f617b75
challomark <challomark@gmail.com>
2023-02-27 18:28:48 +0300

    Practice on Relational Operators

---
+++ c_concepts/0x03-relational_operators/README.md
@@ -0,0 +1,2 @@
+# Practice on Relational Operators
+### Always remember that true = 1, false = 0
---
+++ c_concepts/0x03-relational_operators/relational.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Learning Relational Operators
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int result = 5 < 3;
+	printf("%d\n", result);
+
+	return (0);
+}
