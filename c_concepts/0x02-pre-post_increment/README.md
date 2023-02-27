23d7c29f2a4b6445058eeda696bab7793cb88ed1
challomark <challomark@gmail.com>
2023-02-27 17:35:11 +0300

    Post and Pre Increment

---
+++ c_concepts/0x02-pre-post_increment/README.md
@@ -0,0 +1,4 @@
+# Pre & Post Increment
+
+## ++i Pre Increment 
+## i++ Post Increment
---
+++ c_concepts/0x02-pre-post_increment/example.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Practicing pre and post increment
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	printf("%d\n", i++);
+	return (0);
+}
