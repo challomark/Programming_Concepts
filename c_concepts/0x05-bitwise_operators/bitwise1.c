70427140a561f7129fbb836827d40aea10080cfa
challomark <challomark@gmail.com>
2023-02-28 12:14:11 +0300

    Left and Right Shift Operators

---
+++ c_concepts/0x05-bitwise_operators/bitwise1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using left right shift operator
+ *
+ */
+int main(void)
+{
+	int x = 16;
+	int result = x << 2;
+	
+	printf("%d\n", result);
+	return (0);
+}
