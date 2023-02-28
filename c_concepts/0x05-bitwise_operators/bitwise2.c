eb8d9a5be844076927e39067f54d797ec467dbd3
challomark <challomark@gmail.com>
2023-02-28 12:30:52 +0300

    Using right shift operator

---
+++ c_concepts/0x05-bitwise_operators/bitwise2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Using right shift operator
+ *
+ * Return: Always (0)
+ */
+int main(void)
+{
+	int x = 49;
+	int result = x >> 3;
+
+	printf("%d\n", result);
+	return (0);
+}
