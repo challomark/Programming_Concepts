0caa3a162280bc070d3e678d0fe84a091fbfea3b
challomark <challomark@gmail.com>
2023-03-01 14:15:04 +0300

    Using while loop

---
+++ c_concepts/0x10-while_loop/while_loop.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using the while loop statement
+ *
+ * Return (0);
+ */
+
+int main(void) 
+{
+	int i = 1; /* initialization */
+	
+	while (i <= 5) /* condition */
+	{
+		printf("Hello Challo\n");
+		i++; /* increment or decrement */
+	}
+
+	return (0);
+}
+
+
