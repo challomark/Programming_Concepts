3df017417e1cfe8a368b45f815144daf5b00ba3c
challomark <challomark@gmail.com>
2023-03-01 16:44:32 +0300

    For loop example

---
+++ c_concepts/0x10-loop_control/for_loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using for loop statement
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	for (i = 1; i <= 5; i++)
+	{
+		printf("Hello Mark\n");
+	}
+
+	return (0);
+}
