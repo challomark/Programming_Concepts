74fe7c8dc87a569d4f16ce7c8e69f9e8f0166327
challomark <challomark@gmail.com>
2023-03-01 15:38:51 +0300

    An example

---
+++ c_concepts/0x10-loop_control/do-while_loop.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: using do while loop statement
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 6; /* initialization */
+
+	do {
+		printf("Hello Challo\n");
+		i++; /* increment or decrement */
+	} while (i <= 7); /* condition */
+
+	return (0);
+}
