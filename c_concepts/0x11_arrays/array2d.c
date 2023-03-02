b22def54991bd57f97af1e4f66f6d49e8d3289e9
challomark <challomark@gmail.com>
2023-03-03 00:12:27 +0300

    Finished code

--- c_concepts/0x11_arrays/array2d.c
+++ c_concepts/0x11_arrays/array2d.c
@@ -9,6 +9,23 @@
  */
 int main(void)
 {
-	int d[3][4];
+	int i, j;
 
-	
+	int d[3][4] =
+	{
+		{1,2,3,4},
+		{3,5,7,9},
+		{8,5,2,1},
+
+	};
+
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 4; j++)
+		{
+			printf("%d ", d[i][j]);
+		}
+		printf("\n");
+	}
+	return (0);
+}	
