7d3064bad2d9f34c87a5ed95caef63dc4c32b1bd
challomark <challomark@gmail.com>
2023-03-05 23:09:50 +0300

    More info

--- c_concepts/0x12-pointers/pointer2.c
+++ c_concepts/0x12-pointers/pointer2.c
@@ -8,7 +8,10 @@ int main()
 	ptr = &x; /* prints address of x */
 
 
-	printf("x is: %d\n", x); /* print the value of x */
+	printf("x is: %d\n", x); /* print the value of x which is 5 */
 	printf("memory addess of x is: %p\n", ptr); /* address where x is located */
+
+	/* how to get the variable memory address */
+	printf("%d\n", *ptr); /* value at the address ptr(&x) */
 	return (0);
 }
