a70e256775208142eb68231e85548a6c72f5007f
challomark <challomark@gmail.com>
2023-03-05 22:51:09 +0300

    A simple example

---
+++ c_concepts/0x12-pointers/pointer.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	int x; /* normal variable of type int */
+	int *ptr; /* declaring a pointer */
+	x = 5;
+
+	printf("x is: %d\n", x); /* print the value of */
+	return (0);
+}
