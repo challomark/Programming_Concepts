15f9c8da74a1587a97fb65287d4408bbf5b1e20e
challomark <challomark@gmail.com>
2023-03-07 12:55:04 +0300

    Using header files

---
+++ c_concepts/0x14-header_files/addition.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "funky.h"
+
+int main()
+{
+	int result = add(6, 4);
+	printf("Result is: %d\n", result);
+	return (0);
+}
---
+++ c_concepts/0x14-header_files/funky.h
@@ -0,0 +1,6 @@
+#ifndef FUNKY_H
+#define FUNKY_H
+
+int add(int num1, int num2);
+
+#endif
