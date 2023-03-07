04c059fe33e346e4553d76b54f637f71b91e5f25
challomark <challomark@gmail.com>
2023-03-07 11:58:46 +0300

    Global vs local variable

---
+++ c_concepts/0x10-loop_control/function-variables.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/* global variable*/
+int g = 20;
+
+int main(void)
+{
+	/* local variable */
+	int a = 5;
+	int b = 10;
+	printf("Variable a = %d, b = %d, and g = %d\n", a, b, g);
+
+	return (0);
+}
