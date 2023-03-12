ec38ad2ea0c214632a160f245f9ffe025d61a456
challomark <challomark@gmail.com>
2023-03-12 16:03:33 +0300

    This was fun whiteboarding!

--- c_concepts/0x03-relational_operators/relational2.c
+++ c_concepts/0x03-relational_operators/relational2.c
@@ -2,8 +2,13 @@
 
 int main()
 {
-	int a = 18, b = 9, c, d, e = 10;
+	int a = 18, b = 9, c, d, e = 10, f;
+	c = b++;
+	d = b;
+	f = a > b > d < c;
 
+
+	printf("%d", f != 1);
 	printf("%d\n", a + c == b >= e < c + d != 1);
 	return (0);
 }
