6d1aac9fee2708ec18b5764a01c4a1ea8079a099
challomark <challomark@gmail.com>
2023-04-02 07:36:20 +0300

    Pointer example

--- c_concepts/0x12-pointers/pointerintro.c
+++ c_concepts/0x12-pointers/pointerintro.c
@@ -2,13 +2,16 @@
 
 void main()
 {
-	int a = 10, b = 9;
+	int a = 10, b = 9, c;
 	int *p, *q;
-	p = &a;
-	q = &b;
+	p = &a, &b;
+	/*q = &b */
+	c = *p;
 
 	printf("Value of a is: %d\n", a);
 	printf("Value of a is: %d\n", *p);
 	printf("Address of a is: %p\n", &a);
 	printf("Address of a is: %p\n", p);
+	printf("Address of p is: %p\n", &p);
+	printf("Value of c is: %d\n", c);
 }
