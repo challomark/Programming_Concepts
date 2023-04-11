c657f9c4c272f504c6990041ae6293a4c1af5437
challomark <challomark@gmail.com>
2023-04-11 16:47:02 +0300

    Pointer addition

--- c_concepts/0x12-pointers/pointeradd.c
+++ c_concepts/0x12-pointers/pointeradd.c
@@ -3,9 +3,13 @@
 void main()
 {
 	int a[5] = {1, 4, 2 , -8, 0};
-	int *p = &a[0];
+	unsigned int *p = &a[0];
 
 	printf("Value is: %d\n", *p);
+	printf("Address of element is: %p\n", p);
+	
 	p = p + 2;
+
 	printf("Value is: %d\n", *p);
+	printf("Address of element is: %p\n", p);
 }
