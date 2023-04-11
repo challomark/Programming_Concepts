5cb17c407e0454e498234ed924b2ce16746cb41f
challomark <challomark@gmail.com>
2023-04-11 15:50:45 +0300

    Double pointer example

--- c_concepts/0x12-pointers/doublepointer.c
+++ c_concepts/0x12-pointers/doublepointer.c
@@ -3,9 +3,11 @@
 void main()
 {
 	int a = 10;
-	int *p = &a;
-	int **q = &p;
-	int ***r = &q;
+	unsigned int *p = &a;
+	unsigned int **q = &p;
+	unsigned int ***r = &q;
 
 	printf("a is %d %d %d %d\n", a, *p, **q, ***r);
+	printf("address of q is: %p\n", &q);
+	printf("address of p is: %p\n", &p);
 }
