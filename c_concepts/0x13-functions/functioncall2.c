6ac6288d868896892f7525e3a9a389ee4a834ee7
challomark <challomark@gmail.com>
2023-05-15 18:49:01 +0300

    Function call by reference example

---
+++ c_concepts/0x13-functions/functioncall2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void fun(int *, int *);
+void main()
+{
+	int x = 5, y = 7;
+	fun (&x, &y);
+	printf("Inside main (calling function)\n"); /* calling function */
+	printf("x = %d, y = %d\n", x, y);
+}
+
+void fun(int *x, int *y)
+{
+	*x = 7;
+	*y = 5;
+	printf("Inside fun (called function)\n"); /* called function */
+	printf("x = %d, y = %d\n", *x, *y);
+}
