7c3a9abcd4e359e613d21aed57e7e213ea3d64c7
challomark <challomark@gmail.com>
2023-05-23 16:54:20 +0300

    With argument, no return type

---
+++ c_concepts/0x13-functions/withArgnoRT.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void sum(float, float);
+
+void main()
+{
+	float a, b;
+	printf("Enter two float numbers: ");
+	scanf("%f %f", &a, &b);
+	sum(a , b);
+}
+
+void sum(float x, float y)
+{
+	float s = 0;
+	s = x + y;
+	printf("Sum = %f\n", s);
+}
