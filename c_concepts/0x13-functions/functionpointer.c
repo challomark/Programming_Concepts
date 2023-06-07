fba85dec149c6342cceafe1d1ea70dd104531dab
challomark <challomark@gmail.com>
2023-06-07 15:52:08 +0300

    Pointer to function

---
+++ c_concepts/0x13-functions/functionpointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int sum(int, int);
+
+void main()
+{
+	int s = 0;
+	int (*ptr)(int, int) = &sum;
+	s = (*ptr)(1, 2);
+	printf("Sum = %d\n", s);
+}
+
+int sum(int a, int b)
+{
+	return a + b;
+}
