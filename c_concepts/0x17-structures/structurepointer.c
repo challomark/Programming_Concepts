60964608e7595e7e210d3516a1fa4dac43a6db6e
challomark <challomark@gmail.com>
2023-06-23 12:45:46 +0300

    Structure Pointer example

---
+++ c_concepts/0x17-structures/structurepointer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct student
+{
+	int rollno;
+	char name[20];
+	float marks;
+};
+
+void main()
+{
+	struct student s1 = {1, "Mark", 75};
+	struct student *ptr = &s1;
+
+	printf("Information for s1");
+	printf("\n%d %s %f", &s1.rollno, s1.name, s1.marks);
+}
