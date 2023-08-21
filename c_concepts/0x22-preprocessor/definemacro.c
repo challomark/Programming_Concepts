9be23be10450c3e355599b9c48382e40f25ccc7d
challomark <challomark@gmail.com>
2023-08-21 15:52:17 +0300

    #define examples

---
+++ c_concepts/0x22-preprocessor/definemacro.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#define A 10
+
+int main()
+{
+	int x = A;
+	printf("%d\n", x);
+
+	return 0;
+}
---
+++ c_concepts/0x22-preprocessor/definemacro2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#define MUL(a, b) a * b
+
+int main()
+{
+	printf("Multiplication is %d\n", MUL(5, 2));
+	
+	return 0;
+}
